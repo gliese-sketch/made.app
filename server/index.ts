e50545b45db10ba6da0857948384705f103d0d7f
gliese-sketch <173663566+gliese-sketch@users.noreply.github.com>
2025-02-16 07:14:06 +0000

    Create express application

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,16 @@
-console.log(Bun.env.PORT);
+import http from "node:http";
+import express from "express";
+import { Server } from "socket.io";
+
+const PORT = Bun.env.PORT || 3001;
+
+const app = express();
+const server = http.createServer(app);
+
+app.get("/", (req, res) => {
+  res.send("Socket.io server is healthy!");
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`);
+});
