64f56016f8e0d3b55c145563018aa531f6812368
gliese-sketch <173663566+gliese-sketch@users.noreply.github.com>
2025-02-16 07:34:04 +0000

    create new socket code

--- server/index.ts
+++ server/index.ts
@@ -11,6 +11,13 @@ app.get("/", (req, res) => {
   res.send("Socket.io server is healthy!");
 });
 
+// SOCKET.io
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log(`Socket connected with id: ${socket.id}`);
+});
+
 server.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
 });
